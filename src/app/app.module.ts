f35ac37107462292187a1254d3aa94b3dcc1a4ec
Alejandro <ridim86@gmail.com>
2018-08-01 02:10:42 -0500

    algo similar a la pagina de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    MainComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
